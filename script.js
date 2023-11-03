87d6462f24a9bc7c6123f774eb90907ccb382de3
Kartikey Jaiswal <karan74jaiswal@gmail.com>
2023-11-03 06:38:54 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,75 @@
+//
+const baseUrl = `http://127.0.0.1:3000`;
+
+document.addEventListener("DOMContentLoaded", async function () {
+  const inputText = document.getElementById("input-text");
+  const voiceSelector = document.getElementById("voice-selector");
+  const convertButton = document.getElementById("convert-button");
+  const audioPlayer = document.getElementById("audio-player");
+  const downloadButton = document.getElementById("download-button");
+
+  // Llena el selector de voces
+  // Fill the voice selector with available voices from the API endpoint
+  await fetch(`http://127.0.0.1:3000/api/voices`) // Esta ruta debe coincidir con la que configuraste en server.js
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      data.forEach((voice) => {
+        const option = document.createElement("option");
+        option.value = voice.name;
+        option.dataset.gender = voice.gender;
+        option.dataset.languageCode = voice.languageCode;
+        option.textContent = voice.displayName;
+        voiceSelector.appendChild(option);
+      });
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+
+  convertButton.addEventListener("click", async function (e) {
+    e.preventDefault();
+    const text = inputText.value;
+    const selectedOption = voiceSelector.options[voiceSelector.selectedIndex];
+    const selectedVoice = selectedOption.value;
+    const selectedGender = selectedOption.dataset.gender;
+    const selectedLanguageCode = selectedOption.dataset.languageCode;
+    console.log(selectedVoice);
+    console.log(selectedGender);
+    // Realiza una solicitud a tu API personalizada para convertir texto a voz
+    // Converting text to speech
+    await fetch(`http://127.0.0.1:3000/api/convert`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        text: text,
+        voice: selectedVoice,
+        gender: selectedGender,
+        languageCode: selectedLanguageCode,
+      }),
+    })
+      .then((response) => response.blob())
+      .then((blob) => {
+        console.log(blob);
+        audioPlayer.src = URL.createObjectURL(blob);
+        audioPlayer.play();
+
+        // Download Functionality
+        downloadButton.addEventListener("click", () => {
+          const url = URL.createObjectURL(blob);
+          const a = document.createElement("a");
+          a.href = url;
+          a.download = "converted_audio.mp3"; // You can change the filename if needed
+          a.style.display = "none";
+          document.body.appendChild(a);
+          a.click();
+          URL.revokeObjectURL(url);
+        });
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  });
+});
---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const cors = require("cors");
+const { TextToSpeechClient } = require("@google-cloud/text-to-speech");
+const app = express();
+const port = 3000;
+
+// use it before all route definitions
+app.use(cors({ origin: "*" }));
+
+app.use(express.json());
+
+// Configura la autenticación de Google Cloud Text-to-Speech
+const client = new TextToSpeechClient({
+  keyFilename: "../animated-canyon-403420-aee5ce8ac09e.json",
+});
+
+// Ruta para obtener la lista de voces
+app.get("/api/voices", async (req, res) => {
+  try {
+    // Realiza una solicitud a Google Cloud Text-to-Speech para obtener la lista de voces
+    const [result] = await client.listVoices({});
+    const voices = result.voices;
+    const voiceNames = voices.map((voice) => ({
+      name: voice.name,
+      gender: voice.ssmlGender,
+      displayName: `${voice.name} - ${voice.ssmlGender}`,
+      languageCode: voice.languageCodes[0],
+    }));
+    res.json(voiceNames);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "An error occurred." });
+  }
+});
+
+// Ruta para convertir texto a voz
+app.post("/api/convert", async (req, res) => {
+  // ... Código para convertir texto a voz ...
+  console.log(req.body);
+  const request = {
+    input: { text: req.body.text },
+    voice: {
+      name: req.body.voice,
+      languageCode: req.body.languageCode,
+      ssmlGender: req.body.gender,
+    },
+    audioConfig: { audioEncoding: "MP3" },
+  };
+
+  const [response] = await client.synthesizeSpeech(request);
+  const audioContent = response.audioContent;
+  res.setHeader("Content-Type", "audio/mpeg");
+  res.send(audioContent);
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
