e99be20b7e7e070c04f07de00e56892feac95f65
Kartikey Jaiswal <karan74jaiswal@gmail.com>
2023-11-03 13:23:15 +0530

    fixed vercel bugs

--- server.js
+++ server.js
@@ -18,6 +18,11 @@ const client = new TextToSpeechClient({
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 // Ruta para obtener la lista de voces
+
+app.get("/", (req, res) => {
+  const indexPath = `${__dirname}/index.html`;
+  res.sendFile(indexPath);
+});
 app.use(
   express.static(__dirname, {
     setHeaders: (res, path) => {
@@ -27,11 +32,6 @@ app.use(
     },
   })
 );
-app.get("/", (req, res) => {
-  const indexPath = `${__dirname}/index.html`;
-  res.sendFile(indexPath);
-});
-
 app.get("/api/voices", async (req, res) => {
   try {
     // Realiza una solicitud a Google Cloud Text-to-Speech para obtener la lista de voces
