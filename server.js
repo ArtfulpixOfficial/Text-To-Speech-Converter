2a245270e37817efd8b9e0abd66312bde0251d91
Kartikey Jaiswal <karan74jaiswal@gmail.com>
2023-11-03 11:45:21 +0530

    fixed server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ import express, { json } from "express";
 import cors from "cors";
 import { TextToSpeechClient } from "@google-cloud/text-to-speech";
 const app = express();
-const port = 3000;
 
 // use it before all route definitions
 app.use(cors({ origin: "*" }));
@@ -53,6 +52,7 @@ app.post("/api/convert", async (req, res) => {
   res.send(audioContent);
 });
 
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server is listening on port ${port}`);
+// });
+export default app;
