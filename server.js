8e581cef3db1ba117302668322105e2e1036b344
Kartikey Jaiswal <karan74jaiswal@gmail.com>
2023-11-03 13:44:54 +0530

    fixed vercel bugs

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 // Ruta para obtener la lista de voces
 
-app.use(express.static(__dirname, "js"));
+app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
   const indexPath = `${__dirname}/index.html`;
